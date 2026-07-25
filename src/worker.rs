0510b40ce57bc18015306589e28cd562c486b060
wangtinghu <1992919981@qq.com>
2026-07-24 19:31:20 -0700

    test: cover OOXML table resources

--- src/worker.rs
+++ src/worker.rs
@@ -6391,7 +6391,7 @@ mod tests {
             .expect("document");
         writer
             .write_all(
-                br#"<w:document xmlns:w="urn:w" xmlns:r="urn:r"><w:body><w:p><w:r><w:t>Hello</w:t></w:r></w:p><w:p><w:hyperlink r:id="rIdHyperlink"><w:r><w:t>Link text</w:t></w:r></w:hyperlink></w:p></w:body></w:document>"#,
+                br#"<w:document xmlns:w="urn:w" xmlns:r="urn:r"><w:body><w:p><w:r><w:t>Hello</w:t></w:r></w:p><w:p><w:hyperlink r:id="rIdHyperlink"><w:r><w:t>Link text</w:t></w:r></w:hyperlink></w:p><w:tbl><w:tr><w:tc><w:p><w:r><w:t>Table cell</w:t></w:r></w:p></w:tc></w:tr></w:tbl></w:body></w:document>"#,
             )
             .expect("document bytes");
         writer
@@ -6507,7 +6507,7 @@ mod tests {
             .expect("slide");
         writer
             .write_all(
-                br#"<p:sld xmlns:p="urn:ppt" xmlns:a="urn:dml" xmlns:r="urn:r"><p:cSld><p:spTree><a:p><a:r><a:t>Slide title</a:t></a:r><a:hlinkClick r:id="rIdHyperlink"/></a:p></p:spTree></p:cSld><p:timing><p:tnLst><p:par/></p:tnLst></p:timing></p:sld>"#,
+                br#"<p:sld xmlns:p="urn:ppt" xmlns:a="urn:dml" xmlns:r="urn:r"><p:cSld><p:spTree><a:p><a:r><a:t>Slide title</a:t></a:r><a:hlinkClick r:id="rIdHyperlink"/></a:p><a:tbl><a:tr><a:tc><a:txBody><a:p><a:r><a:t>Table cell</a:t></a:r></a:p></a:txBody></a:tc></a:tr></a:tbl></p:spTree></p:cSld><p:timing><p:tnLst><p:par/></p:tnLst></p:timing></p:sld>"#,
             )
             .expect("slide bytes");
         writer
@@ -8794,6 +8794,8 @@ trailer
             .expect("document xml");
         assert!(document.contains("你好，LinguaMesh！"));
         assert!(document.contains("r:id=\"rIdHyperlink\""));
+        assert!(document.contains("<w:tbl>"));
+        assert!(document.contains("<w:t>你好，LinguaMesh！</w:t></w:r></w:p></w:tc>"));
         let mut document_relationships = String::new();
         archive
             .by_name("word/_rels/document.xml.rels")
@@ -8999,6 +9001,8 @@ trailer
             .expect("slide xml");
         assert!(slide.contains("你好，LinguaMesh！"));
         assert!(slide.contains("r:id=\"rIdHyperlink\""));
+        assert!(slide.contains("<a:tbl>"));
+        assert!(slide.contains("<a:t>你好，LinguaMesh！</a:t></a:r></a:p></a:txBody>"));
         assert!(slide.contains("<p:timing>"));
         let mut slide_relationships = String::new();
         archive
